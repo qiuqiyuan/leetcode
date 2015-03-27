99712c2b8de364efe946e8ed671cec159a250fb2
Qiyuan Qiu <qiuqiyuan@gmail.com>
2015-03-27 15:05:21 -0400

    use vector as dp matrix

--- qqy/EditDistance/main.cpp
+++ qqy/EditDistance/main.cpp
@@ -1,86 +1,40 @@
 #include<iostream>
 #include<algorithm>
+#include<vector>
 using namespace std;
-
 class Solution {
-
     public:
-
         int minDistance(string word1, string word2) {
-
-            int **dp = createMatrix(word1.size()+1, word2.size()+1);
-
-
-
+            if(word1.size() == 0 || word2.size()==0) 
+                return max(word1.size(), word2.size());
+            vector<vector<int> > dp(word1.size()+1, vector<int>(word2.size()+1, -1));
             //init matrix
-
             for(int i=0; i<=word1.size();i++){
-
                 dp[i][0] = i;
+            }
 
+            for(int i=0; i<=word2.size();i++){
                 dp[0][i] = i;
-
             }
-
-
-
             //perform dp
-
             for(int i=1; i<=word1.size();i++){
-
                 for(int j=1;j<=word2.size();j++){
-
-                    dp[i][j] = 
-
-                        dp[i][j-1] + 1 < dp[i-1][j] + 1 ? 
-
-                        min(dp[i-1][j-1] + (word1[i-1] == word2[j-1] ? 0 : 1), dp[i][j-1] + 1) :
-
-                        min(dp[i-1][j-1] + (word1[i-1] == word2[j-1] ? 0 : 1), dp[i-1][j] + 1);
-
+                    dp[i][j] = dp[i][j-1] + 1 < dp[i-1][j] + 1 ? 
+                        min(dp[i-1][j-1] + (word1[i-1] == word2[j-1] ? 0 : 1), 
+                                dp[i][j-1] + 1) :
+                            min(dp[i-1][j-1] + (word1[i-1] == word2[j-1] ? 0 : 1), 
+                                    dp[i-1][j] + 1);
                 }
-
             }
-
-
-
             return dp[word1.size()][word2.size()];
-
         }
-
-    private:
-
-        int** createMatrix(int sizeX, int sizeY){
-
-            int **res = new int*[sizeX];
-
-            for(int i=0; i<sizeX; i++){
-
-                res[i] = new int[sizeY];
-
-                //init all cell to be -1
-
-
-
-                for(int j=0;j<sizeY;j++){
-
-                    res[i][j] = -1;
-
-                }
-
-            }
-
-            return res;
-
-        }
-
 };
-
 
 int main()
 {
-
     Solution s;
     cout<< s.minDistance("pneumonoultramicroscopicsilicovolcanoconiosis","ultramicroscopic") << endl;
-    cout<< s.minDistance("abc","cba") << endl;
+    cout<<endl;
+    cout<< s.minDistance("abc","cbaasdfawefasdfasdf") << endl;
+    cout<< s.minDistance("spartan","part") << endl;
 }
