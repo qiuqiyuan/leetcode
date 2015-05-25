e5836b99acab48a983dd0c0abba6ff045a52138d
Qiyuan Qiu <qiuqiyuan@gmail.com>
2015-05-25 19:37:17 -0400

    Add TwoSums Leetcode 1 solution
    This solution is an updated version of the O(n^2) version.
    This version uses a hash table to remember locations.

---
+++ qqy/TwoSums/main.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<assert.h>
+#include<string>
+#include<algorithm>
+#include<vector>
+using namespace std;
+class Solution {
+    void swap(int&a, int&b){
+        int t = a;
+        a = b;
+        b = t;
+    }
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        map<int, int> mymap;
+        for(int i=0;i<nums.size();i++){
+            int val = nums[i];
+            int loc = i;
+            mymap[val] = loc;
+        }
+        
+        for(int i=0;i<nums.size();i++){
+            int cnt = nums[i];
+            int rest = target - cnt;
+            if(mymap.find(rest) != mymap.end() && mymap[rest] != i){
+                int small = i;
+                int large = mymap[rest];
+                if(small > large){
+                    swap(small, large);
+                }
+                ans.push_back(small+1);
+                ans.push_back(large+1);
+                return ans;
+            }
+        }
+    }
+};
+
+int main()
+{
+    Solution s;
+    return 0; 
+}
