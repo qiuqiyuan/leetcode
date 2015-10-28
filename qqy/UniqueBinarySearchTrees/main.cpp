5acc0f88fe4678d1daedc06446a20f810eb5ad17
Qiyuan Qiu <qiuqiyuan@gmail.com>
2015-10-28 12:08:26 -0400

    update solution and note

--- qqy/UniqueBinarySearchTrees/main.cpp
+++ qqy/UniqueBinarySearchTrees/main.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int numTrees(int n) {
-        int *dp = new int[n+1];
+        int *dp = new int[n+1]();
         dp[0] = 0;
         dp[1] = 1;
         for(int i=2;i<=n;i++){
@@ -11,6 +11,16 @@ public:
             }
             dp[i] = 2*dp[i-1] + st;
         }
+        return dp[n];
+    }
+
+    int numTrees1(int n){
+        int *dp = new int[n+1]();
+        dp[0] = 1;
+        dp[1] = 1;
+        for( int i=2; i<=n; i++)
+            for(int j=0; j<i; j++)
+                dp[i] += dp[j] * dp[i-1-j];
         return dp[n];
     }
 }; 
--- qqy/UniqueBinarySearchTrees/note.md
+++ qqy/UniqueBinarySearchTrees/note.md
@@ -1,3 +1,4 @@
 What have you learned that can be used for the future via this problem?
     - If the problem statement is using 1-indexed, stick your answer with it.
     - Use small examples to verify ideas is always helpful.
+    - `new` will not initialize memory location, use `new int[5]();` instead;
